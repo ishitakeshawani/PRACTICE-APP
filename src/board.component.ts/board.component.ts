caeadd9c8c711da5807f5c649e2a049b42f52c27
Ishita <ishita.k@turvo.com>
2024-05-10 23:02:31 +0530

    Angular Template Syntax: Use Angular's template syntax to render dynamic content and data.

--- src/board.component.ts/board.component.ts
+++ src/board.component.ts/board.component.ts
@@ -10,4 +10,16 @@ import { CommonModule } from '@angular/common';
 })
 export class BoardComponent {
   list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  imageSrc="https://via.placeholder.com/150"
+  isDisabled= true;
+  Submit="Submit"
+  show=true
+  multiClass="blue bold h3"
+  arrMultiClass=["blue","h3"]
+
+  onClick(){
+    alert("clicked!")
+  }
+
+  
 }
