f121ec901c8475fcf6646ad8bfa76806d40f56fb
Ishita <ishita.k@turvo.com>
2024-05-22 20:24:14 +0530

    Added two way data binding

--- src/board.component.ts/board.component.ts
+++ src/board.component.ts/board.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrl: './board.component.css',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
 })
 export class BoardComponent {
   list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -16,9 +17,15 @@ export class BoardComponent {
   show=true
   multiClass="blue bold h3"
   arrMultiClass=["blue","h3"]
+  email:string="ishita@gmail.com"
 
   onClick(){
     alert("clicked!")
+  }
+
+  onChange(e:any){
+    console.log(e.target.value);
+    
   }
 
   
